d31cbfd879c7c32281e0d92a78781a4bf75d4796
pooyan <pooyanmajdabadi@candyspace.com>
2023-04-02 09:37:49 +0100

    Set up initially main activity and splash screen

---
+++ app/src/main/java/com/pooyan/dev/guesscardgame/GuessCardGameApplication.kt
@@ -0,0 +1,7 @@
+package com.pooyan.dev.guesscardgame
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class GuessCardGameApplication: Application()
--- app/src/main/java/com/pooyan/dev/guesscardgame/MainActivity.kt
+++ app/src/main/java/com/pooyan/dev/guesscardgame/MainActivity.kt
@@ -3,41 +3,77 @@ package com.pooyan.dev.guesscardgame
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.SystemUiController
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.pooyan.dev.guesscardgame.ui.theme.GuessCardGameTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContent {
-            GuessCardGameTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
+        // Keep the splash screen on-screen until the UI state is loaded. This condition is
+        // evaluated each time the app needs to be redrawn so it should be fast to avoid blocking
+        // the UI.
+        splashScreen.setKeepOnScreenCondition {
+            //TODO add logic to keep splash screen
+            false
+        }
+
+        // Turn off the decor fitting system windows, which allows us to handle insets,
+        // including IME animations
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent { DrawContent() }
+    }
+}
+
+@Composable
+private fun DrawContent() {
+    val systemUiController = rememberSystemUiController()
+    HandleSystemBarsColor(systemUiController = systemUiController)
+
+    GuessCardGameTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(modifier = Modifier.safeContentPadding()) {
+                //TODO add navHost
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private fun HandleSystemBarsColor(
+    systemUiController: SystemUiController
+) {
+    val isDark = isSystemInDarkTheme()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GuessCardGameTheme {
-        Greeting("Android")
+    SideEffect {
+        systemUiController.setSystemBarsColor(
+            color = Color.Transparent,
+            darkIcons = !isDark
+        )
     }
 }
